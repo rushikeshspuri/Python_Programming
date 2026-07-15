5d2c7942db0c503d5fd1adf8ea411e36253f3704
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-07-16 03:24:21 +0530

    Add files via upload

---
+++ Practice/OOP3.py
@@ -0,0 +1,27 @@
+class Demo:
+    # Class variables
+    Value1 = 10
+    Value2 = 20
+
+    def __init__(self):
+        self.No1 = 11
+        self.No2 = 21
+    
+    # Instance methode
+    def fun(self):
+        print("Inside instance method named as fun")
+        print(self.No1)
+        print(self.No2)
+        print(Demo.Value1)
+        print(Demo.Value2)
+
+    @classmethod
+    def gun(cls):
+        print("Inside class method named as gun")
+        #print(Demo.No1)    Not allowed
+        #print(Demo.No2)    Not allowed 
+        print(Demo.Value1)
+        print(Demo.Value2)
+
+#call without object
+Demo.gun()
