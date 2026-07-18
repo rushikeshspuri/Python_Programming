b197e4f860cb8f6a8bc30baf9b559d360054c5ce
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-07-18 13:03:56 +0530

    Add files via upload

---
+++ Practice/InstanceVariable2.py
@@ -0,0 +1,21 @@
+class Marvellous:
+    # Class variable
+    No1 = 11
+    No2 = 12
+    
+    # Constructor
+    def __init__(self):
+        #Instance Variable
+        self.Value1 = 21
+        self.Value2 = 51
+
+print(Marvellous.No1)
+print(Marvellous.No2)
+
+# Object / Instance Creation
+mobj1 = Marvellous()
+mobj2 = Marvellous()
+mobj3 = Marvellous()
+
+print(mobj1.Value1)
+print(mobj2.Value1)
