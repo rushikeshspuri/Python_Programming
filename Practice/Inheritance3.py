02c94cf50bca257602567585c1791f2c200d67c5
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-06 19:39:19 +0530

    Add files via upload

---
+++ Practice/Inheritance3.py
@@ -0,0 +1,10 @@
+class Base:
+    def __init__(self):
+        print("Inside Base Constructor")
+
+class Derived(Base):
+    def __init__(self):
+        super().__init__()
+        print("Inside Derived Constructor")
+
+dobj = Derived()
