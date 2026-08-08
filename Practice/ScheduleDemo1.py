73ec128f8a917a9ea759dd9078367cbd39bd0df4
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-08-08 17:13:16 +0530

    Add files via upload

---
+++ Practice/ScheduleDemo1.py
@@ -0,0 +1,9 @@
+import schedule
+import time
+import datetime
+
+def main():                     #use to see current time
+    print(f"current time is : {datetime.datetime.now()}")
+
+if __name__ == "__main__":
+    main()
