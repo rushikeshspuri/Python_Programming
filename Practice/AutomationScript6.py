19e5a9c6601cc921d3f0dfb6ada61818739f03d3
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-09-08 17:46:19 +0530

    Add files via upload

---
+++ Practice/AutomationScript6.py
@@ -0,0 +1,29 @@
+import sys
+
+def main():
+    Border = "-"*50
+    print(Border)
+    print("MARVELLOUS AUTOMATION SCRIPT")
+    print(Border)
+    
+    if(len(sys.argv) == 2):
+        if(sys.argv[1] == "--h" or sys.argv[1] =="--H"):
+            print("This automation script is used to travel the directory")
+            print("For better usage please check --u Flag")
+        elif(sys.argv[1] == "--u" or sys.argv[1] ==  "--U"):
+            print("PLease Execute the script as ")
+            print("python FileName.py DiretoryName")
+            print("Directory name should be absolute path")
+        else:
+            DirectoryName = sys.argv[1]
+            print("Directory name is : ",DirectoryName)
+    else: 
+        print("Invalid number of arguments")
+        print("please --h or --u for more information")
+
+    print(Border)
+    print(" ThankYou for using MARVELLOUS AUTOMATION SCRIPT ")
+    print(Border)
+
+if __name__ == "__main__":
+    main()
