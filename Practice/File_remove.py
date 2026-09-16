a83c4b6c1c8569b0a67ae6e3ec5dbaf11770f47e
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-09-16 10:53:31 +0530

    Add files via upload

---
+++ Practice/File_remove.py
@@ -0,0 +1,12 @@
+import os
+
+def main():
+    try:
+        # fobj.remove not applicable
+        os.remove("Demo.txt")
+
+    except FileNotFoundError as fobj:
+        print("File is not present in current directory")
+
+if __name__ == "__main__":
+    main()
