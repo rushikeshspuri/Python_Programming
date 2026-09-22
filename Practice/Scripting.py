66e86308e33ea513188943ad19185395f431f94c
Rushikesh Puri <rushikesh.puri7499@gmail.com>
2026-09-22 10:42:25 +0530

    Add files via upload

---
+++ Practice/Scripting.py
@@ -0,0 +1,13 @@
+print("Enter first Number")
+No1 = int(input())
+
+print("Enter second Number")
+No2 = int(input())
+
+Ans = No1 + No2
+
+print("Addition is :",Ans)
+
+Ans = No1 - No2
+
+print("Subtraction is :",Ans)
